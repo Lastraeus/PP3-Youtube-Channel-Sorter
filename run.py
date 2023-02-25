a336b8fce063eef63178f2591e8335f0d9817afd
Cian Lane <cianlane@gmail.com>
2023-02-25 23:50:33 +0000

    add: skeleton of youtube api requesting

--- run.py
+++ run.py
@@ -1,6 +1,32 @@
+"""
+Query template from 
+https://medium.com/mcd-unison/youtube-data-api-v3-in
+-python-tutorial-with-examples-e829a25d2ebd#5999
+"""
+import googleapiclient.discovery
 import secret_keys
-key = secret_keys.key1
 
-print("Welcome to the YouTube Channel Sorter")
-channel_url = input("Please input a channel URL \n")
-print(f'The chosen url is {channel_url}')
+test_channel1 = "https://www.youtube.com/@kaptainkristian"
+test_channel2 = "https://www.youtube.com/@billwurtz"
+
+# API information
+api_service_name = "youtube"
+api_version = "v3"
+# API key
+DEVELOPER_KEY = secret_keys.key1
+
+# print("Welcome to the YouTube Channel Sorter")
+# inputted_url = input("Please input a channel URL \n")
+# print(f'The chosen url is {inputted_url}')
+
+# API client
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+# 'request' variable is the only thing you must change
+# depending on the resource and method you need to use
+# in your query
+request = None #add request here when channel ID in hand
+
+# Query execution
+response = request.execute()
+# Print the results
+print(response)
