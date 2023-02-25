692406d5cf20bf802f373c64d8016504bce96619
Cian Lane <cianlane@gmail.com>
2023-02-25 20:21:37 +0000

    add: store user prompt test

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 import secret_keys
 key = secret_keys.key1
-print(key)
+
+print("Welcome to the YouTube Channel Sorter")
+channel_url = input("Please input a channel URL \n")
+print(f'The chosen url is {channel_url}')
