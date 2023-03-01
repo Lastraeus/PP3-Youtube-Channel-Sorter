423e5a058e6183dcf4be431f9777c4aa3efc806b
Cian Lane <cianlane@gmail.com>
2023-03-01 23:52:15 +0000

    formatting code, minor refactors

--- run.py
+++ run.py
@@ -1,53 +1,61 @@
 import math
-import json 
+import json
 import datetime
-import googleapiclient.discovery
-import pytube
-import pyinputplus as pyip
-import saveresults
-import gspread
-"""specifically https://github.com/felipeucelli/pytube.git
-for modern channelurl parsing"""
 from os import remove
+
+import pytube  # specifically https://github.com/felipeucelli/pytube.git
+import pyinputplus as pyip
+import googleapiclient.discovery
 from googleapiclient.errors import HttpError
 from dateutil.relativedelta import relativedelta
 from dateutil import parser
+
+import saveresults
 from ascii import logo1
-from google.oauth2.service_account import Credentials
 
-
-# INITIAL VARIABLES -------------------------------------------------------------------------------
+# INITIAL VARIABLES ----------------------------------------------------------
 api_service_name = "youtube"
 api_version = "v3"
 f = open("yt_creds.json")
 api_key_data = json.load(f)
 DEVELOPER_KEY = api_key_data["key1"]
-SAMPLE_RETURN_DATE = parser.parse("2022-09-19T18:08:46Z")  # used to get correct timezone for comparison
+SAMPLE_RETURN_DATE = parser.parse("2022-09-19T18:08:46Z")
+# used to get correct timezone for comparison
+
 ACCEPTED_TIMEFRAMES = ["y", "s", "m", "w"]
 live_now = datetime.datetime.now  # run as live_now()
 api_response_meta = {}
 full_vid_list = []
-last_page_token = ""
-need_next_page = False
-youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+youtube = googleapiclient.discovery.build(
+    api_service_name,
+    api_version,
+    developerKey=DEVELOPER_KEY
+    )
 vid_id_list = []
 vids_in_target_time = []
-line_string = "-" * 80  # default width of template terminal is 80 chars
+LINE_STRING = "-" * 80  # Max width of template terminal is 80 chars
 DEFAULT_SORT_ORDER = 'views'
 DEFAULT_NUM_OF_RESULTS = 3
-saved_playlist_id = ""
 
-# Welcome and Prompt Fucntion Section -------------------------------------------------------------
+# Welcome and Prompt Fucntion Section ----------------------------------------
 
 
 def print_initial_screen():
+    """Prints The Logo and Initial details of app"""
     print(logo1)
     print("Welcome to the YouTube Channel Sorter")
     print("Created by Cian Lane\n")
-    print("See https://github.com/Lastraeus/PP3-Youtube-Channel-Sorter \nfor README and full credits\n\n")
+    print("See https://github.com/Lastraeus/PP3-Youtube-Channel-Sorter")
+    print("for README and full credits\n\n")
+
+    print("Hint: Right Click and copy/paste is all that works here :(\n\n")
 
 
 def channel_prompt():
+    """Asks for a channel url to sort, then uses pytube
+    to validate any possible valid YouTube URL
+    Returns the channel ID if found with pytube
+    if the search failed it lets the user know to try again"""
     inputted_url = input("Please input a valid channel URL \n")
 
     try:
@@ -56,31 +64,34 @@ def channel_prompt():
         print("Channel Found")
         channel_all_vid_playlist_id = channel_id[:1] + "U" + channel_id[1 + 1:]
         return channel_all_vid_playlist_id
-    except:
+    except Exception():
         print("That is not a valid YouTube Channel URL")
 
 
 def timeframe_prompt():
-    print(
-"""
+    """Asks the user to select a valid timeframe.
+    Keeps asking until one is received
+    returns that timeframe"""
+    print("""
 Choose timeframe for most views; \n
 Last (Y)ear, Last (S)ix Months, Last (M)onth, Last (W)eek
-"""
-    )
+""")
     selected_timeframe = input("Enter a letter:\n")
     selected_timeframe = selected_timeframe.lower()
     if selected_timeframe in ACCEPTED_TIMEFRAMES:
-        tf = calculate_past_timeframes(selected_timeframe)
-        print(line_string)
+        timeframe = calculate_past_timeframes(selected_timeframe)
+        print(LINE_STRING)
         print("Loading Selected Results")
         print("This can take a minute if the channel has many videos")
-        return tf
+        return timeframe
 
     print(f'{selected_timeframe} is not in {ACCEPTED_TIMEFRAMES}')
     print("please enter one of the bracketed letters to select")
 
 
 def calculate_past_timeframes(input_letter):
+    """Accepts the selected timeframe letter and converts that to a
+    datetime unit"""
     if input_letter == "y":
         one_year_ago = live_now() - relativedelta(years=+1)
         print('You selected: "Last Year"')
@@ -105,21 +116,20 @@ def calculate_past_timeframes(input_letter):
         print("Unknown Timeframe passed")
 
 
+# Response Parsing Section ---------------------------------------------------
 
 
-
-# Response Parsing Section ------------------------------------------------------------------------
 def get_total_channel_vids(response):
+    """searches the api response json for the total video count on the channel"""
     total_channel_videos = response["pageInfo"]["totalResults"]
     return total_channel_videos
 
 
-def get_total_vids_in_timeframe(vids_in_target_time):
-    result = len(vids_in_target_time)
-    return result
-
-
 def get_oldest_date_in_response(response):
+    """searches the api response json for the odlest video published date in the list of items.
+    can check the last item in any batch response list, 
+    as well as the last video in the trimmed results within timeframe"""
+
     if type(response) is list:
         last_video_on_page = response[-1]
         oldest_datetime = parser.parse(last_video_on_page["published"])
@@ -127,22 +137,32 @@ def get_oldest_date_in_response(response):
     else:
         returned_videos = response["items"]
         last_video_on_page = returned_videos[-1]
-        oldest_datetime = parser.parse(last_video_on_page["snippet"]["publishedAt"])
+        oldest_datetime = parser.parse(
+            last_video_on_page["snippet"]["publishedAt"]
+            )
         return oldest_datetime
 
 
 def add_response_vids_to_list(response):
+    """After a playlist query is searched, add the found video json items
+    to the full_vid_list"""
     returned_videos = response["items"]
     for i in returned_videos:
         full_vid_list.append(i)
 
 
-def date_format_to_google_dates(target_date, SAMPLE_RETURN_DATE):
-    target_date_tz = target_date.astimezone(SAMPLE_RETURN_DATE.tzinfo)
+def date_format_to_google_dates(target_date, sample_date):
+    """converts a standard datetime object to the timezone format
+    that google api results return in"""
+    target_date_tz = target_date.astimezone(sample_date.tzinfo)
     return target_date_tz
 
 
 def grab_ids_in_date(target_date):
+    """strips the ids from the playlist items in full_vid_list
+    only grabs the ones in target_date range
+    adds them to the vid_id_list for a 'video list query'
+    to get significantly more details on each"""
     for item in full_vid_list:
         item_datetime = parser.parse(item["snippet"]["publishedAt"])
         if item_datetime > target_date:
@@ -157,13 +177,17 @@ def get_credits_used(total_videos):
     return quota_credits_used
 
 
-# Output Results After Searching/Parsing Section --------------------------------------------------------------
+# Output Results After Searching/Parsing Section -----------------------------
 def output_results(results, response, last_date):
+    """combines and formats the gathered data and then outputs it for the user.
+    offers them a choice of saving the full result as a google drive link"""
     total_channel_vids = get_total_channel_vids(response)
-    total_vids_in_timeframe = get_total_vids_in_timeframe(vids_in_target_time)
+    total_vids_in_timeframe = len(vids_in_target_time)
     quota_used = get_credits_used(total_vids_in_timeframe)
 
-    vids_in_target_time.sort(key=lambda vid: vid[DEFAULT_SORT_ORDER], reverse=True)
+    vids_in_target_time.sort(
+        key=lambda vid: vid[DEFAULT_SORT_ORDER],
+        reverse=True)
 
     output_header_string = make_header_string(
         total_channel_vids,
@@ -176,9 +200,9 @@ def output_results(results, response, last_date):
         DEFAULT_SORT_ORDER,
         DEFAULT_NUM_OF_RESULTS
         )
-        
+
     print(terminal_output_results_string)
-    print(line_string)
+    print(LINE_STRING)
     print('Would you like to save the full list of results to a text file?')
     print('Enter y/n')
     yesno = pyip.inputYesNo(default="no")
@@ -187,20 +211,33 @@ def output_results(results, response, last_date):
         newfile = saveresults.string_to_txt_file(full_output_string)
         try:
             saveresults.upload_file_to_gdrive(newfile, "txt")
-            remove(newfile) 
+            remove(newfile)
         except Exception as e:
             print(e)
 
 
-def make_header_string(total_channel_vids, total_vids_in_timeframe, last_date, quota_used):
+def make_header_string(
+        total_channel_vids,
+        total_vids_in_timeframe,
+        last_date,
+        quota_used):
+    """Combines meta info for the search and makes a header string to
+    go at the top of the result output. Returns the string"""
+
     output_header_list = []
-    output_header_list.append(line_string)
-    output_header_list.append(f'Channel has {total_channel_vids} total visible videos\n')
-    output_header_list.append(f'Channel has {total_vids_in_timeframe} videos in selected timeframe\n')
-    output_header_list.append(f'Oldest Video in Selected Timeframe uploaded to channel was posted:')
+    output_header_list.append(LINE_STRING)
+    output_header_list.append(
+        f'Channel has {total_channel_vids} total visible videos\n'
+        )
+    output_header_list.append(
+        f'Channel has {total_vids_in_timeframe} videos in selected timeframe\n'
+        )
+    output_header_list.append(
+        'Oldest Video in Selected Timeframe uploaded to channel was posted:'
+        )
     output_header_list.append(f'{last_date}')
     output_header_list.append(f'Total API Quota Credits used: {quota_used}')
-    output_header_list.append(line_string)
+    output_header_list.append(LINE_STRING)
     output_header_string = "\n".join(output_header_list)
     return output_header_string
 
@@ -208,21 +245,28 @@ def make_header_string(total_channel_vids, total_vids_in_timeframe, last_date, q
 def make_terminal_results_string(
     output_header_string,
     order=DEFAULT_SORT_ORDER,
-    num_of_output_results=DEFAULT_NUM_OF_RESULTS,
+    total_output_results=DEFAULT_NUM_OF_RESULTS,
 ):
+    """Takes the defaults and strings needed to format results for the terminal and then
+    combines them and returns it"""
 
     terminal_output_part_list = []
 
-    settings = (f'Top {num_of_output_results} Results\nSorted by {order}:\n{line_string}')
+    settings = (
+        f'Top {total_output_results} Results\nSorted by: {order}'
+        )
     output_results_list = []
-    for video in vids_in_target_time[:num_of_output_results]:
+    for video in vids_in_target_time[:total_output_results]:
         output_results_list.append(video["title"])
-        output_results_list.append(f'Views: {video["views"]}, Published: {video["published"]}')
+        output_results_list.append(
+            f'Views: {video["views"]}, Published: {video["published"]}'
+            )
         output_results_list.append(f'{video["url"]}\n')
     terminal_output_results_string = "\n".join(output_results_list)
 
     terminal_output_part_list.append(output_header_string)
     terminal_output_part_list.append(settings)
+    terminal_output_part_list.append(LINE_STRING)
     terminal_output_part_list.append(terminal_output_results_string)
     terminal_output_string = "\n".join(terminal_output_part_list)
 
@@ -230,11 +274,15 @@ def make_terminal_results_string(
 
 
 def make_full_results_string(output_header_string):
+    """Takes the defaults and strings needed to format results for the full list of results and then
+    combines them and returns it"""
     output_results_list = []
 
     for video in vids_in_target_time:
         output_results_list.append(video["title"])
-        output_results_list.append(f'Views: {video["views"]}, Published: {video["published"]}')
+        output_results_list.append(
+            f'Views: {video["views"]}, Published: {video["published"]}'
+            )
         output_results_list.append(f'{video["url"]}\n')
     full_output_results_string = "\n".join(output_results_list)
 
@@ -247,18 +295,23 @@ def make_full_results_string(output_header_string):
 
 
 def handle_error_reason(error_reason):
+    """handles api error cases;
+    playlistNotFound for a channel with no videos
+    and any error mentioning quota incase a limit is reached"""
     if error_reason == "playlistNotFound":
-        print("Unfortunately this channel/playlist doesn't seem to have any videos")
+        print("""Unfortunately this channel/playlist
+            doesn't seem to have any videos
+            """)
         print("Please try again or choose another")
         print("This time... maybe try one with videos that need sorting\n")
-    
+
     if "quota" in error_reason.lower():
-        if (DEVELOPER_KEY == api_key_data["key1"]):
-            DEVELOPER_KEY = api_key_data["key2"]
-        else:
-            print("Issue with API Quota Limit. Maybe Try Again Tomorrow.")
+        print("Issue with API Quota Limit. Try Again Tomorrow when it resets.")
+
 
 def ask_restart():
+    """asks the user if they would like to search again, if not
+    it then ends the program"""
     print('Would you like to run another search? y/n')
     yesno = pyip.inputYesNo(default="no")
     if yesno == "no":
@@ -266,67 +319,26 @@ def ask_restart():
         exit()
 
 
-# Utility Function Section ------------------------------------------------------------------------
-def divide_chunks(l, n):
-    # looping till length l
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+# Utility Function Section --------------------------------------------------
+def divide_chunks(list_to_divide, max):
+    """Divides a list of items into a list of smaller lists of size max
+    used to divide id_lists that are too long for googles multi_video list
+    api query"""
+    for i in range(0, len(list_to_divide), max):
+        yield list_to_divide[i:i + max]
 
 
-# Sheets/Gspread API Section ----------------------------------------------------------------------
-def load_main_sheet():
-    SCOPE = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive.file",
-        "https://www.googleapis.com/auth/drive"
-        ]
-
-    CREDS = Credentials.from_service_account_file('drive_creds.json')
-    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open('youtube_channel_sorter')
-
-    main_sheet = SHEET.worksheet('main')
-
-    data = main_sheet.get_all_values()
-
-    return data
-
-# TODO def check_quota():
-
-
-# Drive/Pydrive2 API Section ----------------------------------------------------------------------
-def load_drive():
-    scope = ["https://www.googleapis.com/auth/drive"]
-    gauth = GoogleAuth()
-    gauth.auth_method = 'service'
-    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('drive_creds.json', scope)
-    drive = GoogleDrive(gauth)
-
-    about = drive.GetAbout()
-    return about
-
-    # print('Current user name:{}'.format(about['name']))
-    # print('Root folder ID:{}'.format(about['rootFolderId']))
-    # print('Total quota (bytes):{}'.format(about['quotaBytesTotal']))
-    # print('Used quota (bytes):{}'.format(about['quotaBytesUsed']))
-
-
-    # file_list = drive.ListFile().GetList()
-    # for file1 in file_list:
-    #     print('title: %s, id: %s' % (file1['title'], file1['id']))
-
-
-# YouTube Query API Functions Section -------------------------------------------------------------
+# YouTube Query API Functions Section ----------------------------------------
 def query_playlistitems_api(playlist_id, token=None):
-    play_list_request = youtube.playlistItems().list(        
+    play_list_request = youtube.playlistItems().list(
         part="snippet,contentDetails",
         maxResults=50,
         playlistId=playlist_id,
         pageToken=token
     )
-    
-    # https://stackoverflow.com/questions/23945784/how-to-manage-google-api-errors-in-python
+
+    # https://stackoverflow.com/questions/23945784/how-to
+    # -manage-google-api-errors-in-python
     # If the error is a rate limit or connection error,
     # wait and try again.
     # if err.resp.status in [403, 500, 503]:
@@ -337,18 +349,20 @@ def query_playlistitems_api(playlist_id, token=None):
         return r
     except HttpError as err:
         if err.resp.get('content-type', '').startswith('application/json'):
-            reason = json.loads(err.content).get('error').get('errors')[0].get('reason')
+            reason = json.loads(err.content).get('error').get('errors')[0].get(
+                'reason'
+                )
             handle_error_reason(reason)
             return None
 
 
-def query_vids(vid_id_list):
+def query_vids(id_list):
     nextPageToken = None
-    if len(vid_id_list) <= 50:
+    if len(id_list) <= 50:
         while True:
             vid_request = youtube.videos().list(
                 part="snippet,statistics",
-                id=','.join(vid_id_list),
+                id=','.join(id_list),
                 maxResults=50
             )
 
@@ -375,7 +389,7 @@ def query_vids(vid_id_list):
             if not nextPageToken:
                 break
     else:
-        list_of_vid_id_lists = list(divide_chunks(vid_id_list, 50))
+        list_of_vid_id_lists = list(divide_chunks(id_list, 50))
         split_vid_list_query(list_of_vid_id_lists)
 
 
@@ -386,20 +400,22 @@ def split_vid_list_query(list_of_vid_id_lists):
         query_vids(vid_id_list)
 
 
-# Main() Section ----------------------------------------------------------------------------------
+# Main() Section -------------------------------------------------------------
 def main():
     while True:
         print_initial_screen()
         r = None
-        while r == None:
+        while r is None:
             channel_playlist_id = channel_prompt()
             saved_playlist_id = channel_playlist_id
             target_date = None
-            while target_date == None:
+            while target_date is None:
                 target_date = timeframe_prompt()
-            target_date = date_format_to_google_dates(target_date, SAMPLE_RETURN_DATE)
+            target_date = date_format_to_google_dates(
+                target_date,
+                SAMPLE_RETURN_DATE)
             r = query_playlistitems_api(channel_playlist_id)
-        original_response = r  # Save for general channel/playlist metadata parsing
+        original_response = r  # Save for general channel/playlist metadata
         oldest_response_datetime = get_oldest_date_in_response(r)
         add_response_vids_to_list(r)
         while oldest_response_datetime > target_date:
@@ -412,10 +428,12 @@ def main():
                 break
         grab_ids_in_date(target_date)
         query_vids(vid_id_list)
-        last_video_date_in_timeframe = get_oldest_date_in_response(vids_in_target_time)
+        last_video_date_in_timeframe = get_oldest_date_in_response(
+            vids_in_target_time
+            )
         output_results(
-            vids_in_target_time, 
-            original_response, 
+            vids_in_target_time,
+            original_response,
             last_video_date_in_timeframe
             )
         ask_restart()
@@ -423,4 +441,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
